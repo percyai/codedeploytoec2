07eda56a3cbd862d63a4664af5d75d5b4a0a40f5
percyai <104569383+percyai@users.noreply.github.com>
2022-08-23 11:57:12 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+sudo yum update -y
+su - ec2-user -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash"
+su - ec2-user -c ". ~/.nvm/nvm.sh"
+su - ec2-user -c "nvm install 14.16.0"
+su - ec2-user -c "npm install pm2 -g"
+sudo yum install git -y
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
